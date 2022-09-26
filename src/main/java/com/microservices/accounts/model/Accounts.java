6c3ae94e7a446c2b57c2a25fb97d028a8b48a37f
froztfire <jeromegarcia.16@gmail.com>
2022-09-26 23:48:39 +0800

    first commit

---
+++ src/main/java/com/microservices/accounts/model/Accounts.java
@@ -0,0 +1,36 @@
+package com.microservices.accounts.model;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.sql.Date;
+
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Accounts {
+
+    @Column(name = "customer_id")
+    private int customerId;
+
+    @Id
+    @Column(name = "account_number")
+    private Long accountNumber;
+
+    @Column(name = "account_type")
+    private String accountType;
+
+    @Column(name = "branch_address")
+    private String branchAddress;
+
+    @Column(name = "create_dt")
+    private Date createDt;
+
+}
